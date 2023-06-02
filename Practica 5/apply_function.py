079b8e849ea4ecd703fb06a992b6f90903df2ca0
Franco <francogrch@gmail.com>
2023-06-02 12:42:11 -0300

    pract5

--- Practica 5/apply_function.py
+++ Practica 5/apply_function.py
@@ -1,6 +1,11 @@
-def funcion(num):
-    return num % 11
+def funcion1(num):
+    return num % 6
+
+
+def funcion2(num):
+    return num % 5 + 1
 
 
 while True:
-    print(funcion(int(input('Numero: '))))
+    num = int(input('Numero: '))
+    print(f"f1({num}) = {funcion1(num)}\nf2({num}) = {funcion2(num)}")
